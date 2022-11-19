ebc974c3a9e0545882b35bf6febf99bb38938e02
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-19 13:19:08 -0700

    re-structured folders

--- server.js
+++ server.js
@@ -13,23 +13,23 @@ app.listen(PORT, () => console.log("Now listening"));
 
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/pages/index.html'));
 })
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/about.html'));
+    res.sendFile(path.join(__dirname, '/public/pages/about.html'));
 })
 app.get('/work', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/work.html'));
+    res.sendFile(path.join(__dirname, '/public/pages/work.html'));
 })
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/contact.html'));
+    res.sendFile(path.join(__dirname, '/public/pages/contact.html'));
 })
 app.get('/Mickey-N-Friends-Finder', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/portfolio/project-one.html'))
+    res.sendFile(path.join(__dirname, '/public/pages/portfolio/project-one.html'))
 })
 app.get('/Wish-List', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/portfolio/project-two.html'))
+    res.sendFile(path.join(__dirname, '/public/pages/portfolio/project-two.html'))
 })
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/pages/index.html'));
 });

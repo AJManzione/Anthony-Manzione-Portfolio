7b2ba70e57effa1b6c84eb1cf3b989522aa4fc4b
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2023-01-31 21:53:04 -0700

    -push to heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 app.get('/home', (req, res) =>

be8bbdbb85ac4bde65515937dd2d2c7c913df80f
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2023-05-26 23:58:32 -0600

    routes were wrong -fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio_anthony_manzione/',
+  base: '/Anthony_Manzione_Portfolio',
   plugins: [react()],
 });

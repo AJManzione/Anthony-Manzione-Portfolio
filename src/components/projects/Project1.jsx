6ce35263fb2b487547dc797765ae705bc186b154
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2023-05-30 21:00:27 -0600

    enabled yt full screen

--- src/components/projects/Project1.jsx
+++ src/components/projects/Project1.jsx
@@ -156,7 +156,7 @@ function Project1() {
         <h1 style={{color:"white"}} className="title" >PRESENTATION</h1>
         <h5 style={{backgroundColor:"white"}} className='underline'></h5>
       </div>
-      <iframe className='big-movies-borderless' src="https://www.youtube.com/embed/ueMGiAj50o8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className='big-movies-borderless' src="https://www.youtube.com/embed/ueMGiAj50o8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <Footer/>
   </div>

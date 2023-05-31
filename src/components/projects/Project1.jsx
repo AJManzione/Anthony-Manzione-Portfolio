f889a81cad456485e6f4c8d617ccc062e69a21a9
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2023-05-30 18:57:55 -0600

    fixed mobile for project1

--- src/components/projects/Project1.jsx
+++ src/components/projects/Project1.jsx
@@ -23,6 +23,8 @@ function Project1() {
   const refSlideInLeft2 = useAnimationOnScroll('slideInLeft');
   const refSlideInRight3 = useAnimationOnScroll('slideInRight');
   const refSlideInLeft3 = useAnimationOnScroll('slideInLeft');
+  const refSlideInRight4 = useAnimationOnScroll('slideInRight');
+  const refSlideInLeft4 = useAnimationOnScroll('slideInLeft');
   const [loaded, setLoaded] = useState(false);
 
   document.getElementById('three-animation').style.display = 'none';
@@ -87,41 +89,30 @@ function Project1() {
         </div>
       </div>
 
-    <div id='about-MNFF-two'>
-      <div className='col-lg-12 p-5'>
-        <h1 className="title text-center" ref={refSlideInRight}>MICKEY N' FRIENDS FINDER</h1>
-        <h5 className="underline" ref={refSlideInLeft}></h5>
-      </div>
-      <div id='project-one-details' className="col-lg-12 d-flex flex-row justify-content-around flex-wrap mt-5 mb-5">
-        <div className='col-lg-5 p-3' ref={refSlideInLeft2}>
+    <div className='p-5' id='about-MNFF-two'>
+      <div id='project-one-details' className="col-lg-12 d-flex flex-row justify-content-around flex-wrap">
+        <div className='col-lg-5 p-2' ref={refSlideInLeft2}>
           <h3 className='title'>SUMMARY</h3>  
-          <p  >
+          <p>
             Mickey and friends is a collaborative project that was built to allow a user to search for their favorite disney character and get back a list of movies and tv-shows that character appears in, additionally when a user selects a movie or tv-show then they get back information about that movie such as the movie or show poster, the year it was released, a imdb rating, and a brief description of the title.
           </p>
         </div>
-        <div className="col-lg-5 p-3" ref={refSlideInRight2}>
+        <div className="col-lg-5 p-2" ref={refSlideInRight2}>
           <h3 className="title">MY ROLE</h3>
           <p>
             My role in this project as a developer was implimenting the Fetch API method between two Third Party API's including the Disney API and the Omdb API and parsing that JSON content into a dynamic web experience in an interesting way that made since. Connecting these two API's together was a matter of saving the user input and querying the API's with that input. 
           </p>
         </div>
       </div>
-      <svg  
-        style={{backgroundColor:'white'}}
-        preserveAspectRatio="none" viewBox="0 0 100 102" 
-        height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" 
-        class="svgcolor-light">
-        <path d="M0 0 L50 100 L100 0 Z" fill="rgb(236, 224, 224)" stroke="rgb(236, 224, 224)"></path>
-      </svg>
     </div>
 
 
-    <div id='about-MNFF-three'>
+    <div className='m-5' id='about-MNFF-three'>
       <div className='col-lg-12 p-5 d-flex flex-column align-items-center' ref={refSlideInLeft3}>
-        <h1 className="title" >TECHNOLOGY USED</h1>
+        <h1 className="title" ref={refSlideInRight4}>TECHNOLOGY USED</h1>
         <h5 className='underline'></h5>
       </div>
-      <div className='col-lg-12 d-flex flex-column align-items-centerp-1'>  
+      <div className='col-lg-12 d-flex flex-column align-items-center p-3'>  
         <p className='text-center'>Below are some of the key technologies used in the Mickey N' Friends Finder project.</p>
       </div>
       <div className='col-lg-12 d-flex flex-row align-items-center justify-content-around flex-wrap p-5 ' ref={refSlideInRight3}>
@@ -141,16 +132,32 @@ function Project1() {
          <svg xmlns="http://www.w3.org/2000/svg"x="0px" y="0px" height="78" width="80" viewBox="0 0 296.25319 335.67029"><path d="M648.1266,333.9551v2.68536c-.82516,4.51661-.62708,9.10346-.85329,13.65564-.38461,7.74-1.21569,15.43792-1.87068,23.15185q-.75579,8.9013-1.582,17.79762c-.70315,7.71065-1.4335,15.41921-2.07751,23.13473-.62554,7.49413-1.36359,14.97781-1.98753,22.4722-.61147,7.34481-1.35193,14.67961-1.99228,22.0223-.64037,7.34305-1.33,14.68144-1.97709,22.02365q-1.0392,11.79168-2.10933,23.58025c-.6613,7.41582-1.26829,14.83731-1.962,22.25021-.71448,7.63517-1.34169,15.27793-2.05592,22.91324-.68646,7.33851-1.27546,14.687-1.95159,22.02723-.86044,9.34135-1.64274,18.68952-2.51966,28.02961-.682,7.26439-1.27537,14.53766-1.92847,21.8053-.33326,3.70832-.76728,7.40873-.99426,11.12519-.12526,2.05077-1.53417,2.76084-3.16337,3.26-3.9912,1.22271-8.02919,2.28517-12.05241,3.39391q-40.84831,11.25725-81.66728,22.62c-8.09362,2.23487-16.08851,4.9334-24.487,5.93184h-1.79c-14.64877-2.17866-28.56626-7.222-42.80571-10.99381-18.88262-5.00164-37.6594-10.39777-56.48334-15.61834q-9.59114-2.66-19.17988-5.32917c-2.08562-.57718-2.79592-1.75137-2.98875-3.812-1.19362-12.756-2.2937-25.52075-3.43806-38.2807-.98465-10.97908-1.89822-21.96668-2.89167-32.945q-1.68145-18.58154-3.35084-37.165-1.252-14.02029-2.47092-28.04671-1.4201-16.13583-2.90767-32.26816c-.85488-9.12077-1.57981-18.25532-2.41832-27.37817q-1.26794-13.795-2.48279-27.59482-1.004-11.2389-1.9882-22.48151-1.198-13.8012-2.496-27.59434c-.53715-5.74288-.62812-11.51984-.87015-17.28363a25.58836,25.58836,0,0,0-.45923-4.85086v-2.2378c.31072-.2952.19567-.68231.22415-1.03847.0101-.26974-.036-.55616.22337-.75178H647.67908c.2593.19569.21333.48209.22335.75181C647.931,333.27275,647.81582,333.65991,648.1266,333.9551Z" transform="translate(-351.8734 -332.16485)" style={{fill:"#2cfee1"}}/><path d="M500,499.41524q0-69.58725.00049-139.17451c.00006-3.44087.00333-3.44447,3.42727-3.44454q57.05094-.00131,114.1019.00026c3.39185.00011,3.5586.30438,3.21991,3.78078-.77922,7.998-1.44573,16.007-2.17974,24.00948-.836,9.11412-1.71789,18.22407-2.538,27.3396-.67353,7.48606-1.26439,14.97955-1.93126,22.46624-.67335,7.55986-1.411,15.114-2.09116,22.67324-.66026,7.33789-1.27215,14.68009-1.92583,22.01857-.68662,7.7079-1.40945,15.41256-2.09479,23.12057-.65909,7.41273-1.2739,14.82939-1.93869,22.24161-.67138,7.48526-1.3896,14.96631-2.06958,22.4508-.51173,5.63268-.98923,11.26845-1.49326,16.90184-.70287,7.85583-1.42631,15.70977-2.12227,23.56623-.65007,7.33871-1.39222,14.672-1.87439,22.02205-.36825,5.61357-.23572,5.55134-5.54914,7.01922q-40.5261,11.19582-81.03391,22.45781c-3.23257.89627-6.47912,1.74418-9.69746,2.68864-1.55194.45545-2.298.06433-2.21133-1.62118.03444-.6694.00141-1.34221.00141-2.01347Q499.99987,568.66685,500,499.41524Z" transform="translate(-351.8734 -332.16485)" style={{fill:"#1bb39e"}}/><path d="M452.55818,554.13644c.61426-.71623.30346-1.573.30382-2.35546q.02706-60.4142.0581-120.82835c.00279-1.99482-.43641-2.63019-2.54727-2.61215-12.5281.10712-25.05775.02148-37.58637.09109-1.632.00907-2.149-.43931-2.29-2.07877q-1.40869-16.38085-3.04808-32.74124c-.19506-1.92966.32232-2.35292,2.19654-2.34862q39.0411.08955,78.08234-.01291c2.10414-.0066,2.386.70536,2.38478,2.54843q-.06537,101.13784.00515,202.27569c.00143,1.92967-.38155,2.35086-2.32266,1.96193q-44.17471-8.85073-88.37736-17.56178c-1.19142-.2358-1.894-.55683-2.0224-2.05584q-2.31995-27.08028-4.83829-54.14332c-.16692-1.80192.15455-2.42973,2.13479-2.41174q16.33111.14831,32.66411.00213c1.87039-.01582,2.32491.57673,2.46128,2.35962.654,8.54959,1.48111,17.08587,2.2177,25.62937.08546.99116.27354,1.63873,1.46733,1.79505,6.20415.81245,12.398,1.70327,18.59551,2.5668A3.72678,3.72678,0,0,1,452.55818,554.13644Z" transform="translate(-351.8734 -332.16485)" style={{fill:"#747474"}}/><path d="M510.29283,447.18639c0-17.825.03541-35.65015-.04868-53.47474-.00957-2.02649.59322-2.45535,2.51734-2.45167q46.31021.08864,92.62055.00133c1.962-.00365,2.51875.38679,2.3092,2.47465q-1.60843,16.02432-2.85841,32.08313c-.16787,2.19931-.91,2.62082-2.9466,2.6109-16.77874-.08162-33.55815-.01537-50.33694-.08817-1.91311-.00829-2.55528.41159-2.53659,2.4461.102,11.11176.08426,22.22518.01222,33.3374-.01146,1.769.52227,2.266,2.27651,2.25915,15.73485-.06163,31.47025.003,47.20486-.08173,2.11511-.01139,2.79747.3865,2.58587,2.68093q-5.02268,54.4612-9.8574,108.93964a2.50306,2.50306,0,0,1-2.27578,2.66032q-38.17191,8.60764-76.30974,17.36593c-1.78711.41006-2.42383.24807-2.40509-1.84173.10233-11.41009.07786-22.82178.01507-34.23244-.00864-1.5687.41332-2.16981,2.02624-2.45478,13.87257-2.45105,27.72138-5.03653,41.59268-7.495,1.80937-.32069,2.56723-.93011,2.73743-2.90037q1.89634-21.95114,4.075-43.87741c.20026-2.03915-.96892-1.75515-2.15063-1.75529q-22.59587-.00259-45.19175-.00175c-3.0531-.00019-3.055-.0025-3.05506-2.953Q510.29216,473.81209,510.29283,447.18639Z" transform="translate(-351.8734 -332.16485)"/></svg>
         </a>
         <a href="https://disneyapi.dev/">
-          <h3 className='p-2' style={{color:'red'}}>Disney API</h3>
+          <h3 className='p-2' style={{color:'rgb(221, 0, 118)'}}>Disney API</h3>
         </a>
         <a href="https://www.omdbapi.com/">
-          <h3 className='p-2' style={{color:'red'}}>OMDB API</h3>
+          <h3 className='p-2' style={{color:'rgb(221, 0, 118)'}}>OMDB API</h3>
         </a>
         <a href="https://bulma.io/">
           <img style={{width:'200px'}} src="https://bulma.io/images/bulma-logo.png" alt="" />
         </a>
       </div>
-    </div>  
+    </div>
+
+    <svg  
+      style={{backgroundColor:'rgb(46, 46, 58)'}}
+      preserveAspectRatio="none" viewBox="0 0 100 102" 
+      height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" 
+      class="mt-3">
+      <path d="M0 0 L50 100 L100 0 Z" fill="rgb(236, 224, 224)" stroke="rgb(236, 224, 224"></path>
+    </svg>
+    
+    <div id='about-MNFF-four' class="d-flex flex-column justify-content-center align-items-center p-5">
+      <div className='col-lg-12 p-5 d-flex flex-column align-items-center' ref={refSlideInRight4}>
+        <h1 style={{color:"white"}} className="title" >PRESENTATION</h1>
+        <h5 style={{backgroundColor:"white"}} className='underline'></h5>
+      </div>
+      <iframe className='big-movies-borderless' src="https://www.youtube.com/embed/ueMGiAj50o8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     <Footer/>
   </div>
   )
